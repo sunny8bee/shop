4924a5238ba735c901770dcdbfe68f821298a5df
yumi <sunny8bliss@gmail.com>
2022-08-19 19:21:58 +0900

    Create Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from "./logo.svg";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { useState } from "react";
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
 import "./App.css";
+// import Styled from "./components/Styled";
 // import bg from "./img/walnut.png";
 // import almond from "./img/almond.jpg";
+import data from "./data.js";
 
 function App() {
+  let [nuts] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -25,30 +30,37 @@ function App() {
 
       <div className="container">
         <div className="row">
-          <div className="almond">
-            <div className="productDescription">
-              <h4>Almond</h4>
-              <p></p>
-            </div>
-          </div>
-
-          <div className="cashew">
-            <div className="productDescription">
-              <h4>Cashew</h4>
-              <p></p>
-            </div>
-          </div>
-
-          <div className="hodu">
-            <div className="productDescription">
-              <h4>walnut</h4>
-              <p></p>
-            </div>
-          </div>
+          {/* Card 컴포넌트 활용법 */}
+          {/* <Col>
+              <Card nuts={nuts[0]} i={1} />
+            </Col>
+            <Col>
+              <Card nuts={nuts[1]} i={2} />
+            </Col>
+            <Col>
+              <Card nuts={nuts[2]} i={3} />
+            </Col> */}
+          {nuts.map((a, i) => {
+            return <Card nuts={nuts[i]} i={i}></Card>;
+          })}
         </div>
       </div>
     </div>
   );
 }
 
+//컴포넌트 Card 생성과 프롭스 전송
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src={require("./img/nuts" + (props.i + 1) + ".jpg")} />
+      <div className="productDescription">
+        <h5>{props.nuts.title}</h5>
+        <div className="price">
+          <p>{props.nuts.price}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default App;
---
+++ src/data.js
@@ -0,0 +1,28 @@
+// let a = [ 'kim', 20 ] 배열 array
+// let b = { name : 'kim', age : 20 } 오브젝트 자료형
+// b.name 오브젝트 자료형 다루는 법
+
+let data = [
+  {
+    id: 0,
+    title: "Almond",
+    content: "Born in France",
+    price: "12,000",
+  },
+
+  {
+    id: 1,
+    title: "Cashew",
+    content: "Born in Seoul",
+    price: "11,000",
+  },
+
+  {
+    id: 2,
+    title: "walnut",
+    content: "Born in the States",
+    price: "13,000",
+  },
+];
+
+export default data;
